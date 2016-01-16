3a598bc9b119504a12b929116f95296640388b07
Thomas Bertani <mail@thomasbertani.it>
2016-01-16 16:41:37 +0000

    strConcat improvements

--- oraclizeAPI.sol
+++ oraclizeAPI.sol
@@ -170,37 +170,38 @@ contract usingOraclize {
     	}	
     }
 
-    function strConcat(string _a, string _b) internal returns (string) {
-        bytes memory ba = bytes(_a);
-        bytes memory bb = bytes(_b);
-        bytes memory bc = "                                                                                                                                                                                                                              ";
+    function strConcat(string _a, string _b, string _c, string _d, string _e) internal returns (string){
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        bytes memory _bc = bytes(_c);
+        bytes memory _bd = bytes(_d);
+        bytes memory _be = bytes(_e);
+        string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
+        bytes memory babcde = bytes(abcde);
         uint k = 0;
-        for (uint i = 0; i < ba.length; i ++){
-            bc[k] = ba[i];
-            k++;
-        }
-        for (i = 0; i < bb.length; i ++){
-            bc[k] = bb[i];
-            k++;
-        }
-        for (i = k; i < bc.length; i ++){
-            bc[i] = 0;
-        }
-        return string(bc);
+        for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
+        for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
+        for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
+        for (i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
+        for (i = 0; i < _be.length; i++) babcde[k++] = _be[i];
+        return string(babcde);
+    }
+    
+    function strConcat(string _a, string _b, string _c, string _d) internal returns (string) {
+        return strConcat(_a, _b, _c, _d, "");
     }
 
+    function strConcat(string _a, string _b, string _c) internal returns (string) {
+        return strConcat(_a, _b, _c, "", "");
+    }
+
+    function strConcat(string _a, string _b) internal returns (string) {
+        return strConcat(_a, _b, "", "", "");
+    }
 
     // parseInt
     function parseInt(string _a) internal returns (uint) {
-        bytes memory bresult = bytes(_a);
-        uint mint = 0;
-        for (uint i=0; i<bresult.length; i++){
-            if ((bresult[i] >= 48)&&(bresult[i] <= 57)){
-               mint *= 10;
-               mint += uint(bresult[i]) - 48;
-            } else break;
-        }
-       return mint;
+        return parseInt(_a, 0);
     }
 
     // parseInt(parseFloat*10^_b)
