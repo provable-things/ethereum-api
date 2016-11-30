1bc50d770720df5832f2758f3d0d0552c033dbd2
Thomas Bertani <mail@thomasbertani.it>
2016-11-30 19:35:28 +0000

    Add uint2str to utils

--- oraclizeAPI_0.4.sol
+++ oraclizeAPI_0.4.sol
@@ -266,6 +266,24 @@ contract usingOraclize {
         return mint;
     }
     
+    function uint2str(uint i) internal returns (string){
+        if (i == 0) return "0";
+        uint j = i;
+        uint len;
+        while (j != 0){
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (i != 0){
+            bstr[k--] = byte(48 + i % 10);
+            i /= 10;
+        }
+        return string(bstr);
+    }
+    
+    
 
 }
 // </ORACLIZE_API>
