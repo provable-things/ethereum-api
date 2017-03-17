f675df3a8227c5232351ff33074ff892d9c5e62c
polybioz <polybioz@users.noreply.github.com>
2017-03-17 01:02:33 +0100

    Fixed parsing of an address in a checksum format with uppercase characters.

--- oraclizeAPI_0.4.sol
+++ oraclizeAPI_0.4.sol
@@ -353,8 +353,10 @@ contract usingOraclize {
             b1 = uint160(tmp[i]);
             b2 = uint160(tmp[i+1]);
             if ((b1 >= 97)&&(b1 <= 102)) b1 -= 87;
+            else if ((b1 >= 65)&&(b1 <= 70)) b1 -= 55;
             else if ((b1 >= 48)&&(b1 <= 57)) b1 -= 48;
             if ((b2 >= 97)&&(b2 <= 102)) b2 -= 87;
+            else if ((b2 >= 65)&&(b2 <= 70)) b2 -= 55;
             else if ((b2 >= 48)&&(b2 <= 57)) b2 -= 48;
             iaddr += (b1*16+b2);
         }
