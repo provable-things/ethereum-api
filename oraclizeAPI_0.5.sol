1ae72c508d2db244290530f759f20380e38b4e3a
Adam J Lemmon <adamjlemmon@gmail.com>
2017-11-27 10:45:04 -0500

    updated oraclizeI visibility modifiers to match the connector

--- oraclizeAPI_0.5.sol
+++ oraclizeAPI_0.5.sol
@@ -33,19 +33,19 @@ pragma solidity ^0.4.18;
 
 contract OraclizeI {
     address public cbAddress;
-    function query(uint _timestamp, string _datasource, string _arg) public payable returns (bytes32 _id);
-    function query_withGasLimit(uint _timestamp, string _datasource, string _arg, uint _gaslimit) public payable returns (bytes32 _id);
+    function query(uint _timestamp, string _datasource, string _arg) external payable returns (bytes32 _id);
+    function query_withGasLimit(uint _timestamp, string _datasource, string _arg, uint _gaslimit) external payable returns (bytes32 _id);
     function query2(uint _timestamp, string _datasource, string _arg1, string _arg2) public payable returns (bytes32 _id);
-    function query2_withGasLimit(uint _timestamp, string _datasource, string _arg1, string _arg2, uint _gaslimit) public payable returns (bytes32 _id);
+    function query2_withGasLimit(uint _timestamp, string _datasource, string _arg1, string _arg2, uint _gaslimit) external payable returns (bytes32 _id);
     function queryN(uint _timestamp, string _datasource, bytes _argN) public payable returns (bytes32 _id);
-    function queryN_withGasLimit(uint _timestamp, string _datasource, bytes _argN, uint _gaslimit) public payable returns (bytes32 _id);
+    function queryN_withGasLimit(uint _timestamp, string _datasource, bytes _argN, uint _gaslimit) external payable returns (bytes32 _id);
     function getPrice(string _datasource) public returns (uint _dsprice);
     function getPrice(string _datasource, uint gaslimit) public returns (uint _dsprice);
-    function useCoupon(string _coupon) public;
-    function setProofType(byte _proofType) public;
-    function setConfig(bytes32 _config) public;
-    function setCustomGasPrice(uint _gasPrice) public;
-    function randomDS_getSessionPubKeyHash() public returns(bytes32);
+    function useCoupon(string _coupon) public; // no longer present in connector
+    function setProofType(byte _proofType) external;
+    function setConfig(bytes32 _config) public; // no longer present in connector
+    function setCustomGasPrice(uint _gasPrice) external;
+    function randomDS_getSessionPubKeyHash() external constant returns(bytes32);
 }
 contract OraclizeAddrResolverI {
     function getAddress() public returns (address _addr);
