f77d8d650ff7447009a0136fd9a6db4963c49a60
D-Nice <D-Nice@users.noreply.github.com>
2018-04-10 14:46:00 -0400

    update with cbor libraries

--- lib-experimental/oraclizeAPI_lib.sol
+++ lib-experimental/oraclizeAPI_lib.sol
@@ -44,9 +44,241 @@ contract OraclizeI {
     function setCustomGasPrice(uint _gasPrice) external;
     function randomDS_getSessionPubKeyHash() external view returns(bytes32);
 }
+
 contract OraclizeAddrResolverI {
     function getAddress() public view returns (address _addr);
 }
+
+/*
+Begin solidity-cborutils
+
+https://github.com/smartcontractkit/solidity-cborutils
+
+MIT License
+
+Copyright (c) 2018 SmartContract ChainLink, Ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+library Buffer {
+    struct buffer {
+        bytes buf;
+        uint capacity;
+    }
+
+    function init(buffer memory buf, uint capacity) internal pure {
+        if(capacity % 32 != 0) capacity += 32 - (capacity % 32);
+        // Allocate space for the buffer data
+        buf.capacity = capacity;
+        assembly {
+            let ptr := mload(0x40)
+            mstore(buf, ptr)
+            mstore(0x40, add(ptr, capacity))
+        }
+    }
+
+    function resize(buffer memory buf, uint capacity) private pure {
+        bytes memory oldbuf = buf.buf;
+        init(buf, capacity);
+        append(buf, oldbuf);
+    }
+
+    function max(uint a, uint b) private pure returns(uint) {
+        if(a > b) {
+            return a;
+        }
+        return b;
+    }
+
+    /**
+     * @dev Appends a byte array to the end of the buffer. Reverts if doing so
+     *      would exceed the capacity of the buffer.
+     * @param buf The buffer to append to.
+     * @param data The data to append.
+     * @return The original buffer.
+     */
+    function append(buffer memory buf, bytes data) internal pure returns(buffer memory) {
+        if(data.length + buf.buf.length > buf.capacity) {
+            resize(buf, max(buf.capacity, data.length) * 2);
+        }
+
+        uint dest;
+        uint src;
+        uint len = data.length;
+        assembly {
+            // Memory address of the buffer data
+            let bufptr := mload(buf)
+            // Length of existing buffer data
+            let buflen := mload(bufptr)
+            // Start address = buffer address + buffer length + sizeof(buffer length)
+            dest := add(add(bufptr, buflen), 32)
+            // Update buffer length
+            mstore(bufptr, add(buflen, mload(data)))
+            src := add(data, 32)
+        }
+
+        // Copy word-length chunks while possible
+        for(; len >= 32; len -= 32) {
+            assembly {
+                mstore(dest, mload(src))
+            }
+            dest += 32;
+            src += 32;
+        }
+
+        // Copy remaining bytes
+        uint mask = 256 ** (32 - len) - 1;
+        assembly {
+            let srcpart := and(mload(src), not(mask))
+            let destpart := and(mload(dest), mask)
+            mstore(dest, or(destpart, srcpart))
+        }
+
+        return buf;
+    }
+
+    /**
+     * @dev Appends a byte to the end of the buffer. Reverts if doing so would
+     * exceed the capacity of the buffer.
+     * @param buf The buffer to append to.
+     * @param data The data to append.
+     * @return The original buffer.
+     */
+    function append(buffer memory buf, uint8 data) internal pure {
+        if(buf.buf.length + 1 > buf.capacity) {
+            resize(buf, buf.capacity * 2);
+        }
+
+        assembly {
+            // Memory address of the buffer data
+            let bufptr := mload(buf)
+            // Length of existing buffer data
+            let buflen := mload(bufptr)
+            // Address = buffer address + buffer length + sizeof(buffer length)
+            let dest := add(add(bufptr, buflen), 32)
+            mstore8(dest, data)
+            // Update buffer length
+            mstore(bufptr, add(buflen, 1))
+        }
+    }
+
+    /**
+     * @dev Appends a byte to the end of the buffer. Reverts if doing so would
+     * exceed the capacity of the buffer.
+     * @param buf The buffer to append to.
+     * @param data The data to append.
+     * @return The original buffer.
+     */
+    function appendInt(buffer memory buf, uint data, uint len) internal pure returns(buffer memory) {
+        if(len + buf.buf.length > buf.capacity) {
+            resize(buf, max(buf.capacity, len) * 2);
+        }
+
+        uint mask = 256 ** len - 1;
+        assembly {
+            // Memory address of the buffer data
+            let bufptr := mload(buf)
+            // Length of existing buffer data
+            let buflen := mload(bufptr)
+            // Address = buffer address + buffer length + sizeof(buffer length) + len
+            let dest := add(add(bufptr, buflen), len)
+            mstore(dest, or(and(mload(dest), not(mask)), data))
+            // Update buffer length
+            mstore(bufptr, add(buflen, len))
+        }
+        return buf;
+    }
+}
+
+library CBOR {
+    using Buffer for Buffer.buffer;
+
+    uint8 private constant MAJOR_TYPE_INT = 0;
+    uint8 private constant MAJOR_TYPE_NEGATIVE_INT = 1;
+    uint8 private constant MAJOR_TYPE_BYTES = 2;
+    uint8 private constant MAJOR_TYPE_STRING = 3;
+    uint8 private constant MAJOR_TYPE_ARRAY = 4;
+    uint8 private constant MAJOR_TYPE_MAP = 5;
+    uint8 private constant MAJOR_TYPE_CONTENT_FREE = 7;
+
+    function encodeType(Buffer.buffer memory buf, uint8 major, uint value) private pure {
+        if(value <= 23) {
+            buf.append(uint8((major << 5) | value));
+        } else if(value <= 0xFF) {
+            buf.append(uint8((major << 5) | 24));
+            buf.appendInt(value, 1);
+        } else if(value <= 0xFFFF) {
+            buf.append(uint8((major << 5) | 25));
+            buf.appendInt(value, 2);
+        } else if(value <= 0xFFFFFFFF) {
+            buf.append(uint8((major << 5) | 26));
+            buf.appendInt(value, 4);
+        } else if(value <= 0xFFFFFFFFFFFFFFFF) {
+            buf.append(uint8((major << 5) | 27));
+            buf.appendInt(value, 8);
+        }
+    }
+
+    function encodeIndefiniteLengthType(Buffer.buffer memory buf, uint8 major) private pure {
+        buf.append(uint8((major << 5) | 31));
+    }
+
+    function encodeUInt(Buffer.buffer memory buf, uint value) internal pure {
+        encodeType(buf, MAJOR_TYPE_INT, value);
+    }
+
+    function encodeInt(Buffer.buffer memory buf, int value) internal pure {
+        if(value >= 0) {
+            encodeType(buf, MAJOR_TYPE_INT, uint(value));
+        } else {
+            encodeType(buf, MAJOR_TYPE_NEGATIVE_INT, uint(-1 - value));
+        }
+    }
+
+    function encodeBytes(Buffer.buffer memory buf, bytes value) internal pure {
+        encodeType(buf, MAJOR_TYPE_BYTES, value.length);
+        buf.append(value);
+    }
+
+    function encodeString(Buffer.buffer memory buf, string value) internal pure {
+        encodeType(buf, MAJOR_TYPE_STRING, bytes(value).length);
+        buf.append(bytes(value));
+    }
+
+    function startArray(Buffer.buffer memory buf) internal pure {
+        encodeIndefiniteLengthType(buf, MAJOR_TYPE_ARRAY);
+    }
+
+    function startMap(Buffer.buffer memory buf) internal pure {
+        encodeIndefiniteLengthType(buf, MAJOR_TYPE_MAP);
+    }
+
+    function endSequence(Buffer.buffer memory buf) internal pure {
+        encodeIndefiniteLengthType(buf, MAJOR_TYPE_CONTENT_FREE);
+    }
+}
+
+/*
+End solidity-cborutils
+ */
+
 library oraclizeLib {
 
     function proofType_NONE()
@@ -440,49 +672,33 @@ library oraclizeLib {
         return string(bstr);
     }
 
+    using CBOR for Buffer.buffer;
     function stra2cbor(string[] arr)
     internal
     pure
     returns (bytes) {
-        uint arrlen = arr.length;
+        Buffer.buffer memory buf;
+        Buffer.init(buf, 1024);
+        buf.startArray();
+        for (uint i = 0; i < arr.length; i++) {
+            buf.encodeString(arr[i]);
+        }
+        buf.endSequence();
+        return buf.buf;
+    }
 
-        // get correct cbor output length
-        uint outputlen = 0;
-        bytes[] memory elemArray = new bytes[](arrlen);
-        for (uint i = 0; i < arrlen; i++) {
-            elemArray[i] = (bytes(arr[i]));
-            outputlen += elemArray[i].length + (elemArray[i].length - 1)/23 + 3; //+3 accounts for paired identifier types
+    function ba2cbor(bytes[] arr)
+    internal
+    pure
+    returns (bytes) {
+        Buffer.buffer memory buf;
+        Buffer.init(buf, 1024);
+        buf.startArray();
+        for (uint i = 0; i < arr.length; i++) {
+            buf.encodeBytes(arr[i]);
         }
-        uint ctr = 0;
-        uint cborlen = arrlen + 0x80;
-        outputlen += byte(cborlen).length;
-        bytes memory res = new bytes(outputlen);
-
-        while (byte(cborlen).length > ctr) {
-            res[ctr] = byte(cborlen)[ctr];
-            ctr++;
-        }
-        for (i = 0; i < arrlen; i++) {
-            res[ctr] = 0x5F;
-            ctr++;
-            for (uint x = 0; x < elemArray[i].length; x++) {
-                // if there's a bug with larger strings, this may be the culprit
-                if (x % 23 == 0) {
-                    uint elemcborlen = elemArray[i].length - x >= 24 ? 23 : elemArray[i].length - x;
-                    elemcborlen += 0x40;
-                    uint lctr = ctr;
-                    while (byte(elemcborlen).length > ctr - lctr) {
-                        res[ctr] = byte(elemcborlen)[ctr - lctr];
-                        ctr++;
-                    }
-                }
-                res[ctr] = elemArray[i][x];
-                ctr++;
-            }
-            res[ctr] = 0xFF;
-            ctr++;
-        }
-        return res;
+        buf.endSequence();
+        return buf.buf;
     }
 
     function b2s(bytes _b)
--- oraclizeAPI_0.4.sol
+++ oraclizeAPI_0.4.sol
@@ -46,9 +46,241 @@ contract OraclizeI {
     function setCustomGasPrice(uint _gasPrice);
     function randomDS_getSessionPubKeyHash() returns(bytes32);
 }
+
 contract OraclizeAddrResolverI {
     function getAddress() returns (address _addr);
 }
+
+/*
+Begin solidity-cborutils
+
+https://github.com/smartcontractkit/solidity-cborutils
+
+MIT License
+
+Copyright (c) 2018 SmartContract ChainLink, Ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+library Buffer {
+    struct buffer {
+        bytes buf;
+        uint capacity;
+    }
+
+    function init(buffer memory buf, uint capacity) internal pure {
+        if(capacity % 32 != 0) capacity += 32 - (capacity % 32);
+        // Allocate space for the buffer data
+        buf.capacity = capacity;
+        assembly {
+            let ptr := mload(0x40)
+            mstore(buf, ptr)
+            mstore(0x40, add(ptr, capacity))
+        }
+    }
+
+    function resize(buffer memory buf, uint capacity) private pure {
+        bytes memory oldbuf = buf.buf;
+        init(buf, capacity);
+        append(buf, oldbuf);
+    }
+
+    function max(uint a, uint b) private pure returns(uint) {
+        if(a > b) {
+            return a;
+        }
+        return b;
+    }
+
+    /**
+     * @dev Appends a byte array to the end of the buffer. Reverts if doing so
+     *      would exceed the capacity of the buffer.
+     * @param buf The buffer to append to.
+     * @param data The data to append.
+     * @return The original buffer.
+     */
+    function append(buffer memory buf, bytes data) internal pure returns(buffer memory) {
+        if(data.length + buf.buf.length > buf.capacity) {
+            resize(buf, max(buf.capacity, data.length) * 2);
+        }
+
+        uint dest;
+        uint src;
+        uint len = data.length;
+        assembly {
+            // Memory address of the buffer data
+            let bufptr := mload(buf)
+            // Length of existing buffer data
+            let buflen := mload(bufptr)
+            // Start address = buffer address + buffer length + sizeof(buffer length)
+            dest := add(add(bufptr, buflen), 32)
+            // Update buffer length
+            mstore(bufptr, add(buflen, mload(data)))
+            src := add(data, 32)
+        }
+
+        // Copy word-length chunks while possible
+        for(; len >= 32; len -= 32) {
+            assembly {
+                mstore(dest, mload(src))
+            }
+            dest += 32;
+            src += 32;
+        }
+
+        // Copy remaining bytes
+        uint mask = 256 ** (32 - len) - 1;
+        assembly {
+            let srcpart := and(mload(src), not(mask))
+            let destpart := and(mload(dest), mask)
+            mstore(dest, or(destpart, srcpart))
+        }
+
+        return buf;
+    }
+
+    /**
+     * @dev Appends a byte to the end of the buffer. Reverts if doing so would
+     * exceed the capacity of the buffer.
+     * @param buf The buffer to append to.
+     * @param data The data to append.
+     * @return The original buffer.
+     */
+    function append(buffer memory buf, uint8 data) internal pure {
+        if(buf.buf.length + 1 > buf.capacity) {
+            resize(buf, buf.capacity * 2);
+        }
+
+        assembly {
+            // Memory address of the buffer data
+            let bufptr := mload(buf)
+            // Length of existing buffer data
+            let buflen := mload(bufptr)
+            // Address = buffer address + buffer length + sizeof(buffer length)
+            let dest := add(add(bufptr, buflen), 32)
+            mstore8(dest, data)
+            // Update buffer length
+            mstore(bufptr, add(buflen, 1))
+        }
+    }
+
+    /**
+     * @dev Appends a byte to the end of the buffer. Reverts if doing so would
+     * exceed the capacity of the buffer.
+     * @param buf The buffer to append to.
+     * @param data The data to append.
+     * @return The original buffer.
+     */
+    function appendInt(buffer memory buf, uint data, uint len) internal pure returns(buffer memory) {
+        if(len + buf.buf.length > buf.capacity) {
+            resize(buf, max(buf.capacity, len) * 2);
+        }
+
+        uint mask = 256 ** len - 1;
+        assembly {
+            // Memory address of the buffer data
+            let bufptr := mload(buf)
+            // Length of existing buffer data
+            let buflen := mload(bufptr)
+            // Address = buffer address + buffer length + sizeof(buffer length) + len
+            let dest := add(add(bufptr, buflen), len)
+            mstore(dest, or(and(mload(dest), not(mask)), data))
+            // Update buffer length
+            mstore(bufptr, add(buflen, len))
+        }
+        return buf;
+    }
+}
+
+library CBOR {
+    using Buffer for Buffer.buffer;
+
+    uint8 private constant MAJOR_TYPE_INT = 0;
+    uint8 private constant MAJOR_TYPE_NEGATIVE_INT = 1;
+    uint8 private constant MAJOR_TYPE_BYTES = 2;
+    uint8 private constant MAJOR_TYPE_STRING = 3;
+    uint8 private constant MAJOR_TYPE_ARRAY = 4;
+    uint8 private constant MAJOR_TYPE_MAP = 5;
+    uint8 private constant MAJOR_TYPE_CONTENT_FREE = 7;
+
+    function encodeType(Buffer.buffer memory buf, uint8 major, uint value) private pure {
+        if(value <= 23) {
+            buf.append(uint8((major << 5) | value));
+        } else if(value <= 0xFF) {
+            buf.append(uint8((major << 5) | 24));
+            buf.appendInt(value, 1);
+        } else if(value <= 0xFFFF) {
+            buf.append(uint8((major << 5) | 25));
+            buf.appendInt(value, 2);
+        } else if(value <= 0xFFFFFFFF) {
+            buf.append(uint8((major << 5) | 26));
+            buf.appendInt(value, 4);
+        } else if(value <= 0xFFFFFFFFFFFFFFFF) {
+            buf.append(uint8((major << 5) | 27));
+            buf.appendInt(value, 8);
+        }
+    }
+
+    function encodeIndefiniteLengthType(Buffer.buffer memory buf, uint8 major) private pure {
+        buf.append(uint8((major << 5) | 31));
+    }
+
+    function encodeUInt(Buffer.buffer memory buf, uint value) internal pure {
+        encodeType(buf, MAJOR_TYPE_INT, value);
+    }
+
+    function encodeInt(Buffer.buffer memory buf, int value) internal pure {
+        if(value >= 0) {
+            encodeType(buf, MAJOR_TYPE_INT, uint(value));
+        } else {
+            encodeType(buf, MAJOR_TYPE_NEGATIVE_INT, uint(-1 - value));
+        }
+    }
+
+    function encodeBytes(Buffer.buffer memory buf, bytes value) internal pure {
+        encodeType(buf, MAJOR_TYPE_BYTES, value.length);
+        buf.append(value);
+    }
+
+    function encodeString(Buffer.buffer memory buf, string value) internal pure {
+        encodeType(buf, MAJOR_TYPE_STRING, bytes(value).length);
+        buf.append(bytes(value));
+    }
+
+    function startArray(Buffer.buffer memory buf) internal pure {
+        encodeIndefiniteLengthType(buf, MAJOR_TYPE_ARRAY);
+    }
+
+    function startMap(Buffer.buffer memory buf) internal pure {
+        encodeIndefiniteLengthType(buf, MAJOR_TYPE_MAP);
+    }
+
+    function endSequence(Buffer.buffer memory buf) internal pure {
+        encodeIndefiniteLengthType(buf, MAJOR_TYPE_CONTENT_FREE);
+    }
+}
+
+/*
+End solidity-cborutils
+ */
+
 contract usingOraclize {
     uint constant day = 60*60*24;
     uint constant week = 60*60*24*7;
@@ -668,90 +900,28 @@ contract usingOraclize {
         return string(bstr);
     }
 
+    using CBOR for Buffer.buffer;
     function stra2cbor(string[] arr) internal returns (bytes) {
-            uint arrlen = arr.length;
-
-            // get correct cbor output length
-            uint outputlen = 0;
-            bytes[] memory elemArray = new bytes[](arrlen);
-            for (uint i = 0; i < arrlen; i++) {
-                elemArray[i] = (bytes(arr[i]));
-                outputlen += elemArray[i].length + (elemArray[i].length - 1)/23 + 3; //+3 accounts for paired identifier types
-            }
-            uint ctr = 0;
-            uint cborlen = arrlen + 0x80;
-            outputlen += byte(cborlen).length;
-            bytes memory res = new bytes(outputlen);
-
-            while (byte(cborlen).length > ctr) {
-                res[ctr] = byte(cborlen)[ctr];
-                ctr++;
-            }
-            for (i = 0; i < arrlen; i++) {
-                res[ctr] = 0x5F;
-                ctr++;
-                for (uint x = 0; x < elemArray[i].length; x++) {
-                    // if there's a bug with larger strings, this may be the culprit
-                    if (x % 23 == 0) {
-                        uint elemcborlen = elemArray[i].length - x >= 24 ? 23 : elemArray[i].length - x;
-                        elemcborlen += 0x40;
-                        uint lctr = ctr;
-                        while (byte(elemcborlen).length > ctr - lctr) {
-                            res[ctr] = byte(elemcborlen)[ctr - lctr];
-                            ctr++;
-                        }
-                    }
-                    res[ctr] = elemArray[i][x];
-                    ctr++;
-                }
-                res[ctr] = 0xFF;
-                ctr++;
-            }
-            return res;
+        Buffer.buffer memory buf;
+        Buffer.init(buf, 1024);
+        buf.startArray();
+        for (uint i = 0; i < arr.length; i++) {
+            buf.encodeString(arr[i]);
         }
+        buf.endSequence();
+        return buf.buf;
+    }
 
-    function ba2cbor(bytes[] arr) internal returns (bytes) {
-            uint arrlen = arr.length;
-
-            // get correct cbor output length
-            uint outputlen = 0;
-            bytes[] memory elemArray = new bytes[](arrlen);
-            for (uint i = 0; i < arrlen; i++) {
-                elemArray[i] = (bytes(arr[i]));
-                outputlen += elemArray[i].length + (elemArray[i].length - 1)/23 + 3; //+3 accounts for paired identifier types
-            }
-            uint ctr = 0;
-            uint cborlen = arrlen + 0x80;
-            outputlen += byte(cborlen).length;
-            bytes memory res = new bytes(outputlen);
-
-            while (byte(cborlen).length > ctr) {
-                res[ctr] = byte(cborlen)[ctr];
-                ctr++;
-            }
-            for (i = 0; i < arrlen; i++) {
-                res[ctr] = 0x5F;
-                ctr++;
-                for (uint x = 0; x < elemArray[i].length; x++) {
-                    // if there's a bug with larger strings, this may be the culprit
-                    if (x % 23 == 0) {
-                        uint elemcborlen = elemArray[i].length - x >= 24 ? 23 : elemArray[i].length - x;
-                        elemcborlen += 0x40;
-                        uint lctr = ctr;
-                        while (byte(elemcborlen).length > ctr - lctr) {
-                            res[ctr] = byte(elemcborlen)[ctr - lctr];
-                            ctr++;
-                        }
-                    }
-                    res[ctr] = elemArray[i][x];
-                    ctr++;
-                }
-                res[ctr] = 0xFF;
-                ctr++;
-            }
-            return res;
+    function ba2cbor(bytes[] arr) internal pure returns (bytes) {
+        Buffer.buffer memory buf;
+        Buffer.init(buf, 1024);
+        buf.startArray();
+        for (uint i = 0; i < arr.length; i++) {
+            buf.encodeBytes(arr[i]);
         }
-
+        buf.endSequence();
+        return buf.buf;
+    }
 
     string oraclize_network_name;
     function oraclize_setNetworkName(string _network_name) internal {
@@ -765,7 +935,7 @@ contract usingOraclize {
     function oraclize_newRandomDSQuery(uint _delay, uint _nbytes, uint _customGasLimit) internal returns (bytes32){
         if ((_nbytes == 0)||(_nbytes > 32)) throw;
 	// Convert from seconds to ledger timer ticks
-        _delay *= 10; 
+        _delay *= 10;
         bytes memory nbytes = new bytes(1);
         nbytes[0] = byte(_nbytes);
         bytes memory unonce = new bytes(32);
@@ -778,18 +948,18 @@ contract usingOraclize {
             mstore(add(sessionKeyHash, 0x20), sessionKeyHash_bytes32)
         }
         bytes memory delay = new bytes(32);
-        assembly { 
-            mstore(add(delay, 0x20), _delay) 
+        assembly {
+            mstore(add(delay, 0x20), _delay)
         }
-        
+
         bytes memory delay_bytes8 = new bytes(8);
         copyBytes(delay, 24, 8, delay_bytes8, 0);
 
         bytes[4] memory args = [unonce, nbytes, sessionKeyHash, delay];
         bytes32 queryId = oraclize_query("random", args, _customGasLimit);
-        
+
         bytes memory delay_bytes8_left = new bytes(8);
-        
+
         assembly {
             let x := mload(add(delay_bytes8, 0x20))
             mstore8(add(delay_bytes8_left, 0x27), div(x, 0x100000000000000000000000000000000000000000000000000000000000000))
@@ -802,11 +972,11 @@ contract usingOraclize {
             mstore8(add(delay_bytes8_left, 0x20), div(x, 0x1000000000000000000000000000000000000000000000000))
 
         }
-        
+
         oraclize_randomDS_setCommitment(queryId, sha3(delay_bytes8_left, args[1], sha256(args[0]), args[2]));
         return queryId;
     }
-    
+
     function oraclize_randomDS_setCommitment(bytes32 queryId, bytes32 commitment) internal {
         oraclize_randomDS_args[queryId] = commitment;
     }
@@ -899,9 +1069,9 @@ contract usingOraclize {
 
     function matchBytes32Prefix(bytes32 content, bytes prefix, uint n_random_bytes) internal returns (bool){
         bool match_ = true;
-	
+
 	if (prefix.length != n_random_bytes) throw;
-	        
+
         for (uint256 i=0; i< n_random_bytes; i++) {
             if (content[i] != prefix[i]) match_ = false;
         }
