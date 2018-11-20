726b79c15742859524d30283711df26646bb4bb2
Greg Kapka <greg@oraclize.it>
2018-11-20 18:32:56 +0000

    add safeParseInt()

--- oraclizeAPI_0.5.sol
+++ oraclizeAPI_0.5.sol
@@ -911,6 +911,32 @@ contract usingOraclize {
         return strConcat(_a, _b, "", "", "");
     }
 
+    function safeParseInt(string memory _a) internal pure returns (uint) {
+        return safeParseInt(_a, 0);
+    }
+
+    function safeParseInt(string memory _a, uint _b) internal pure returns (uint) {
+        bytes memory bresult = bytes(_a);
+        uint mint = 0;
+        bool decimals = false;
+        for (uint i = 0; i < bresult.length; i++) {
+            if ((uint(uint8(bresult[i])) >= 48) && (uint(uint8(bresult[i])) <= 57)) {
+                if (decimals) {
+                   if (_b == 0) break;
+                    else _b--;
+                }
+                mint *= 10;
+                mint += uint(uint8(bresult[i])) - 48;
+            } else if (uint(uint8(bresult[i])) == 46) {
+                 decimals = true;
+            } else { 
+                revert("Non-numeral character encountered in string!");
+            }
+        }
+        if (_b > 0) mint *= 10 ** _b;
+        return mint;
+    }
+
     // parseInt
     function parseInt(string memory _a) internal pure returns (uint) {
         return parseInt(_a, 0);
