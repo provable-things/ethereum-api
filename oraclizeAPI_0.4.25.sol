0b81ed04f53bc0d0836eb822bdd3e87600383533
Greg Kapka <greg@oraclize.it>
2019-04-17 14:49:32 +0100

    add byte-datasource overloads to interface for 0.4.25API

--- oraclizeAPI_0.4.25.sol
+++ oraclizeAPI_0.4.25.sol
@@ -52,15 +52,21 @@ contract OraclizeI {
     function getPrice(string memory _datasource, address _address) view public returns (uint256 _dsprice);
     function getPrice(string memory _datasource, uint256 _gasLimit) view public returns (uint256 _dsprice);
     function queryN(uint _timestamp, string _datasource, bytes _argN) public payable returns (bytes32 _id);
+    function queryN(uint _timestamp, bytes1 _datasource, bytes _argN) public payable returns (bytes32 _id);
     function query(uint _timestamp, string _datasource, string _arg) external payable returns (bytes32 _id);
+    function query(uint _timestamp, bytes1 _datasource, string _arg) external payable returns (bytes32 _id);
     function requestCallbackRebroadcast(bytes32 _queryId, uint256 _gasLimit, uint256 _gasPrice) payable external;
     function getPrice(byte _datasource, uint256 _gasLimit, address _address) view public returns (uint256 _dsprice);
     function getRebroadcastCost(uint256 _gasLimit, uint256 _gasPrice) pure public returns (uint256 _rebroadcastCost);
     function query2(uint _timestamp, string _datasource, string _arg1, string _arg2) public payable returns (bytes32 _id);
+    function query2(uint _timestamp, bytes1 _datasource, string _arg1, string _arg2) public payable returns (bytes32 _id);
     function getPrice(string memory _datasource, uint256 _gasLimit, address _address) view public returns (uint256 _dsprice);
     function query_withGasLimit(uint _timestamp, string _datasource, string _arg, uint _gaslimit) external payable returns (bytes32 _id);
+    function query_withGasLimit(uint _timestamp, bytes1 _datasource, string _arg, uint _gaslimit) external payable returns (bytes32 _id);
     function queryN_withGasLimit(uint _timestamp, string _datasource, bytes _argN, uint _gaslimit) external payable returns (bytes32 _id);
+    function queryN_withGasLimit(uint _timestamp, bytes1 _datasource, bytes _argN, uint _gaslimit) external payable returns (bytes32 _id);
     function query2_withGasLimit(uint _timestamp, string _datasource, string _arg1, string _arg2, uint _gaslimit) external payable returns (bytes32 _id);
+    function query2_withGasLimit(uint _timestamp, bytes1 _datasource, string _arg1, string _arg2, uint _gaslimit) external payable returns (bytes32 _id);
 }
 
 interface ERC20Interface {
