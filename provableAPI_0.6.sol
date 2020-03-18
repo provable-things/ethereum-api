3322318cb8d6855aa7a1fe1a41bdbc926be2f1c9
allemanfredi <alle.manfredi@gmail.com>
2020-03-18 13:22:40 +0100

    chore: changes supported compiler version

--- provableAPI_0.6.sol
+++ provableAPI_0.6.sol
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-pragma solidity >= 0.5.0 < 0.7.0; // Incompatible compiler version - please select a compiler within the stated pragma range, or use a different version of the provableAPI!
+pragma solidity >= 0.6.0 < 0.7.0; // Incompatible compiler version - please select a compiler within the stated pragma range, or use a different version of the provableAPI!
 
 // Dummy contract only used to emit to end-user they are using wrong solc
 abstract contract solcChecker {
