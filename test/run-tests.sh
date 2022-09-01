505febe3f35352581e43db98208a0a0a13f370ed
Greg Kapka <greg@oraclize.it>
2022-09-01 10:18:27 +0100

    feat(tests): <- install deps in those if required

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -2,6 +2,14 @@
 set -e
 cd "$(dirname -- $0)"
 
+if [ ! -d "../node_modules" ]; then
+  echo '✔ Installing dependencies...'
+  cd ../ && npm ci && cd "$(dirname -- $0)"
+  echo '✔ Dependencies installed!'
+else
+  echo '✔ Dependencies already installed ∴ skipping installation!'
+fi
+
 echo '✔ Running tests...'
 
 ./test-compilation.sh
